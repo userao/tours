3e8d9f0f9e6015c7bb1c23e9bb3790bb1e846165
Andrey O <ostapenko2121@gmail.com>
2026-05-21 11:03:03 +0700

    added route for static assets

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/public' });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
